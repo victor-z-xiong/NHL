2a88d67415d137807893bf18dc0ee5e25b138fcd
lungofd <victorzxiong98@gmail.com>
2018-04-24 00:08:05 -0700

    fixed timezone, format looks okay

--- app/src/main/java/no_name/nhl_app/ScoreBoard.java
+++ app/src/main/java/no_name/nhl_app/ScoreBoard.java
@@ -13,6 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.LinearLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.android.volley.*;
@@ -39,7 +40,7 @@ import java.util.TimeZone;
 public class ScoreBoard extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private static final String url = "https://statsapi.web.nhl.com/api/v1/schedule";
+    private static final String url = "https://statsapi.web.nhl.com/api/v1/schedule?date=2018-04-23";
     private static ScoreBoard scoreBoardInstance;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,52 +85,54 @@ public class ScoreBoard extends AppCompatActivity
             String toDisplay = response.getString("copyright");
             int totalGames = response.getInt("totalGames");
 
-            final TextView[] myTextViews = new TextView[totalGames*2];
-            TextView tv;
 
             LinearLayout ll = (LinearLayout) findViewById(R.id.scoreboard_linear_layout);
-            //setContentView(R.layout.content_score_board);
+
             for(int i = 0; i < totalGames*2; i++){
-                tv = new TextView(this);
-                JSONArray dates = response.getJSONArray("dates");
-                JSONObject elem = null;
-                JSONArray games = null;
-                if(dates != null)
-                    elem = dates.getJSONObject(0);
 
-                if(elem != null)
-                    games = elem.getJSONArray("games");
+                JSONArray games = getGame(response);
+                int score = getScore(i, games);
+                String team = getTeamName(i, games);
+                String gameTime = setGameTime(getGameDate(i, games), i);
 
-                String teamType = "away";
-                String team = "";
-                String gameTime = "";
-                String gameDate = "";
-                int score = 0;
-                if(i % 2 == 0)
-                    teamType = "home";
+                TableRow row = new TableRow(this);
+                LinearLayout llForRow = new LinearLayout(this);
+                android.widget.LinearLayout.LayoutParams params = new android.widget.LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1f);
 
-                if(games != null) {
-                    gameDate = games.getJSONObject(i / 2).getString("gameDate");
-                    team = games.getJSONObject(i / 2).getJSONObject("teams").getJSONObject(teamType).getJSONObject("team").getString("name");
-                    score = games.getJSONObject(i / 2).getJSONObject("teams").getJSONObject(teamType).getInt("score");
+                TextView teamName = new TextView(this);
+                TextView scoreText = new TextView(this);
+                teamName.setText(team);
+                teamName.setId(3*i);
+                teamName.setTextSize(24);
+                teamName.setLayoutParams(params);
+                teamName.setPadding(50, 20, 0, 0);
+                scoreText.setText(Integer.toString(score));
+                scoreText.setId(3*i+1);
+                scoreText.setTextSize(24);
+                scoreText.setPadding(0, 20, 100, 0);
+
+
+                llForRow.addView(teamName);
+                llForRow.addView(scoreText);
+                row.addView(llForRow);
+
+                ll.addView(row);
+                if(i % 2 == 1){
+                    TableRow gameTimeRow = new TableRow(this);
+                    TableRow spacerRow = new TableRow(this);
+                    TextView gameTimeText = new TextView(this);
+                    TextView blankView = new TextView(this);
+                    gameTimeText.setText(gameTime);
+                    gameTimeText.setId(3*i+2);
+                    gameTimeText.setTextSize(18);
+                    gameTimeText.setPadding(50, 0, 0, 0);
+
+                    spacerRow.addView(blankView);
+                    gameTimeRow.addView(gameTimeText);
+                    ll.addView(gameTimeRow);
+                    ll.addView(spacerRow);
                 }
-
-                try {
-                    DateFormat dfPST = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-                    dfPST.setTimeZone(TimeZone.getTimeZone("America/Los_Angeles"));
-                    Date date = dfPST.parse(gameDate);
-                    if(i % 2 == 0)
-                        gameTime = dfPST.format(date).toString();
-                } catch (ParseException e){
-                    System.out.println("bad");
-                }
-
-
-                tv.setText(team + ": " + score+ "        " + gameTime);
-                tv.setId(17+i);
-                ll.addView(tv);
-
-                myTextViews[i] = tv;
 
             }
 
@@ -138,6 +141,93 @@ public class ScoreBoard extends AppCompatActivity
             System.out.println("UNEXPECTED JSON EXCEPTION!");
         }
 
+    }
+    private JSONArray getGame(JSONObject response){
+        try{
+            JSONArray dates = response.getJSONArray("dates");
+            JSONObject elem = null;
+            JSONArray games = null;
+            if(dates != null)
+                elem = dates.getJSONObject(0);
+
+            if(elem != null)
+                games = elem.getJSONArray("games");
+
+            return games;
+        } catch(JSONException e){
+            System.out.println("UNEXPECTED JSON EXCEPTION");
+        }
+
+        return null;
+    }
+    private int getScore(int i, JSONArray games){
+        int score = 0;
+        String teamType = "away";
+        if(i % 2 == 0)
+            teamType = "home";
+        if(games != null) {
+            try {
+                score = games.getJSONObject(i / 2).getJSONObject("teams").getJSONObject(teamType).getInt("score");
+            } catch (JSONException e) {
+                System.out.println("UNEXPECTED JSON EXCEPTION!");
+            }
+        }
+        return score;
+    }
+
+    private String getTeamName(int i, JSONArray games){
+        String team = "";
+        String teamType = "away";
+        if(i % 2 == 0)
+            teamType = "home";
+        if(games != null) {
+            try {
+                team = games.getJSONObject(i / 2).getJSONObject("teams").getJSONObject(teamType).getJSONObject("team").getString("name");
+            } catch (JSONException e) {
+                System.out.println("UNEXPECTED JSON EXCEPTION!");
+            }
+        }
+        return team;
+    }
+
+    private String getGameDate(int i, JSONArray games){
+        String gameDate = "";
+        if(games != null) {
+            try {
+                gameDate = games.getJSONObject(i / 2).getString("gameDate");
+            } catch (JSONException e) {
+                System.out.println("UNEXPECTED JSON EXCEPTION!");
+            }
+        }
+        return gameDate;
+    }
+
+    private String formatGameTime(String gameTime){
+        String hour = gameTime.substring(11,13);
+        if(Integer.parseInt(hour) > 12){
+            hour = Integer.toString(Integer.parseInt(hour) - 12);
+            return hour + gameTime.substring(13,16) + "PM PST";
+        }
+        return hour + gameTime.substring(13,16) + "AM PST";
+    }
+
+    private String setGameTime(String gameDate, int i){
+        String gameTime = "";
+        try {
+            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+            df.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+            Date date = df.parse(gameDate);
+            SimpleDateFormat dfPST = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+            dfPST.setTimeZone(TimeZone.getTimeZone("America/Los_Angeles"));
+            gameTime = dfPST.format(date).toString();
+        } catch (ParseException e){
+            System.out.println("bad");
+        }
+        if(gameTime.length() > 0){
+            return formatGameTime(gameTime);
+        }
+        return gameTime;
     }
 
     @Override
