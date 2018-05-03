0ee56788c3947d146328c681c2d7ff100c6bc4d8
lungofd <victorzxiong98@gmail.com>
2018-05-02 20:27:53 -0700

    splash screen will need to change implementation

--- app/src/main/java/no_name/nhl_app/ScoreBoard.java
+++ app/src/main/java/no_name/nhl_app/ScoreBoard.java
@@ -4,6 +4,7 @@ import android.app.ActionBar;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SwipeRefreshLayout;
